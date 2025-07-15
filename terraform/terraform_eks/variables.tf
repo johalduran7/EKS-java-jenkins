c1955aeaca4682c2c7cfbf678faae9bac6bfab3d
John Duran <johalduran@gmail.com>
2025-07-15 15:33:14 -0500

    updated

--- terraform/terraform_eks/variables.tf
+++ terraform/terraform_eks/variables.tf
@@ -2,8 +2,8 @@ variable "aws_region" {
   default = "us-east-1"
 }
 variable "eks_cluster_name" {
-  default = "eks-cluster"
+  default = "eks-cluster-book"
 }
 variable "eks_node_group_name" {
-  default = "single-node"
+  default = "single-node-book"
 }
